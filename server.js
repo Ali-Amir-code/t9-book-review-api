b463eab189d8b7ffdb2bbb0145c874ce077d226d
Ali Amir <muhammadaliamir24@gmail.com>
2025-08-08 10:21:04 +0500

    add a default route to api.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 import express, { json } from 'express';
 import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';    
+import { fileURLToPath } from 'url';
 import connectDB from './config/db.js';
 
 import authRoutes from './routes/auth.js';
@@ -11,6 +11,7 @@ import booksRoutes from './routes/books.js';
 import reviewsRoutes from './routes/reviews.js';
 
 connectDB();
+
 const app = express();
 app.use(json());
 
@@ -19,6 +20,8 @@ app.use('/uploads', express.static(join(dirname(fileURLToPath(import.meta.url)),
 app.use('/api/auth', authRoutes);
 app.use('/api/books', booksRoutes);
 app.use('/api/books', reviewsRoutes);
+
+app.get('/', (req, res) => res.json({ msg: 'Hello from Book Review API!' }));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
